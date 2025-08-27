8f29862cf0f476f27ad331a23b646a290d1b6c1c
Stanislav Nemchenko <stas.buka@gmail.com>
2025-08-27 16:06:34 +0300

    start files for module 2 part 2

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,47 +1,7 @@
-import Mailbox from "./MailboxProps";
-import Product from "./Product";
-import Books, { type Book } from "./Books";
-import Alert from "./Alert/Alert";
-import Button from "./Button/Button";
-import UserMenu from "./UserMenu/UserMenu";
-import { HiUserCircle, HiOutlineUserGroup } from "react-icons/hi";
-
-const books: Book[] = [
-    { id: "id-1", name: "JS for beginners" },
-    { id: "id-2", name: "React basics" },
-    { id: "id-3", name: "React Query overview" },
-];
-
 export default function App() {
     return (
         <>
-            <h1>Products</h1>
-
-            <Product
-                name="Tacos With Lime"
-                imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
-                price={10.99}
-            />
-            <Product
-                name="Fries and Burger"
-                imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
-                price={14.29}
-            />
-            <Mailbox username="User Mailbox" messages={["Hello", "World"]} />
-            <Mailbox username="User Mailbox" />
-            {books.map((book) => (
-                <Books key={book.id} id={book.id} name={book.name} />
-            ))}
-            <Alert />
-            <Alert type="success" />
-            <Alert type="error" />
-
-            <Button variant="primary" text="Login" />
-            <Button variant="secondary" text="Follow" />
-
-            <UserMenu name="John Doe" icon={HiUserCircle} />
-            <UserMenu name="Jane Smith" icon={HiOutlineUserGroup} />
-            <UserMenu name="Default User" />
+            <h1>Module №2. Part №2</h1>
         </>
     );
 }
